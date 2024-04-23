6a4107068e22e39e64c1716f15e46ec90c04129b
nandobfer <nandobfer@yahoo.com.br>
2024-04-23 11:19:07 -0300

    feat: category post endpoint

--- src/class/Category.ts
+++ src/class/Category.ts
@@ -1,8 +1,12 @@
 import { Prisma } from "@prisma/client"
 import { prisma } from "../prisma"
+import { FileUpload, WithoutFunctions } from "./helpers"
+import { saveFile } from "../tools/saveFile"
+import { uid } from "uid"
 
 export const category_include = Prisma.validator<Prisma.CategoryInclude>()({})
 export type CategoryPrisma = Prisma.CategoryGetPayload<{ include: typeof category_include }>
+export type CategoryForm = Omit<WithoutFunctions<Category>, "id" | "cover"> & { cover?: FileUpload }
 
 export class Category {
     id: string
@@ -15,6 +19,17 @@ export class Category {
         return categories
     }
 
+    static async new(data: CategoryForm) {
+        const category_prisma = await prisma.category.create({ data: { ...data, id: uid(), cover: "" } })
+        const category = new Category(category_prisma)
+
+        if (data.cover) {
+            await category.updateCover(data.cover)
+        }
+
+        return category
+    }
+
     constructor(data: CategoryPrisma) {
         this.load(data)
     }
@@ -23,5 +38,11 @@ export class Category {
         this.id = data.id
         this.name = data.name
         this.cover = data.cover
+    }
+
+    async updateCover(cover: FileUpload) {
+        const url = saveFile(`categories/${this.id}`, cover)
+        const data = await prisma.category.update({ where: { id: this.id }, data: { cover: url } })
+        this.load(data)
     }
 }
--- src/rest/category.ts
+++ src/rest/category.ts
@@ -1,11 +1,23 @@
 import express, { Express, Request, Response } from "express"
-import { Category } from "../class/Category"
+import { Category, CategoryForm } from "../class/Category"
 const router = express.Router()
 
 router.get("/list", async (request: Request, response: Response) => {
     try {
         const categories = await Category.list()
         response.json(categories)
+    } catch (error) {
+        console.log(error)
+        response.status(500).send(error)
+    }
+})
+
+router.post("/", async (request: Request, response: Response) => {
+    const data = request.body as CategoryForm
+
+    try {
+        const category = Category.new(data)
+        response.json(category)
     } catch (error) {
         console.log(error)
         response.status(500).send(error)
