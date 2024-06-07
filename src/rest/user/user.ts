aa926ccf29411bad2983d718f41a16ad6253ab07
nandobfer <nandobfer@yahoo.com.br>
2024-06-07 14:43:45 -0300

    feat: user notifications route

--- src/rest/user/user.ts
+++ src/rest/user/user.ts
@@ -191,4 +191,21 @@ router.post("/creator", async (request: Request, response: Response) => {
     }
 })
 
+router.get("/notifications", async (request: Request, response: Response) => {
+    const user_id = request.query.user_id as string | undefined
+
+    if (user_id) {
+        try {
+            const user = new User(user_id)
+            await user.init()
+            response.json(user.notifications)
+        } catch (error) {
+            console.log(error)
+            response.status(500).send(error)
+        }
+    } else {
+        response.status(400).send("user_id param is required")
+    }
+})
+
 export default router
