940d926f1c63a08fa86e1163f9b2eaeacd83d188
tainarasn <tainarasn.sba@gmail.com>
2024-06-07 14:41:44 -0300

    feat: delete role

--- src/rest/role.ts
+++ src/rest/role.ts
@@ -1,6 +1,6 @@
 import express, { Express, Request, Response } from "express"
 import { prisma } from "../prisma"
-import { Role, RoleForm, RolePrisma } from "../class/Role"
+import { PartialRole, Role, RoleForm, RolePrisma } from "../class/Role"
 
 const router = express.Router()
 
@@ -15,6 +15,29 @@ router.post("/", async (request: Request, response: Response) => {
         console.log(error)
         return response.status(500).send("Error create new role")
     }
+})
+
+// router.post("/update", async (request: Request, response: Response) => {
+//     const data = request.body as Partial<Role>
+
+//     try {
+//         const role = await Role.update(data)
+//         console.log(role)
+//         return response.json(role)
+//     } catch (error) {
+//         console.log(error)
+//         return response.status(500).send("Error create new role")
+//     }
+// })
+
+router.get("/delete", async (request: Request, response: Response) => {
+    const data = request.query.id as number | undefined
+
+    try {
+        const role = await Role.remove(Number(data))
+        console.log(role)
+        return response.json(data)
+    } catch (error) {}
 })
 
 export default router
